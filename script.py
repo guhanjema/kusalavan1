0d91013f2d58e7706f72562d67367d4c7000c24d
guhanjema <85568893+guhanjema@users.noreply.github.com>
2021-06-15 12:07:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ class Script(object):
 I was produced to the TamilTentkotta Group.
 Anyway, the start has not been hit. Subscribe to our official channel below.
                    
-Join Now ➟ Group ♻️ @Tentkottagroup
+Join Now ➟ Group @Tentkottagroup
       
 </b>
 """
@@ -20,7 +20,7 @@ HELP_MSG = """
 """
 
 
-ABOUT_MSG = """⭕️<b>My Name :Thalapathy Filter Bot</b>
+     ABOUT_MSG = """⭕️<b>My Name :Thalapathy Filter Bot</b>
 
 ⭕️<b>Creater :</b> @Vip_offical    
 
